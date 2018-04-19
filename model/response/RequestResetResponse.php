914475740ea4898d08b9c14add789842c71ba4be
Cuong Cao V <cuong.cao@asiantech.vn>
2018-04-20 00:13:17 +0700

    update code

--- model/response/RequestResetResponse.php
+++ model/response/RequestResetResponse.php
@@ -2,8 +2,8 @@
 	class RequestResetResponse {
 
 		function __construct($userId, $userName) {
-			$this->userId = $userId;
-			$this->userName = $userName;
+			$this->user_id = $userId;
+			$this->user_name = $userName;
 		}
 	}
 ?>
